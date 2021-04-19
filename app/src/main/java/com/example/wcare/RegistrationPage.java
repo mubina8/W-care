9ded5fa555f385b2af7bab1222ab9a49c8a10314
Asus <nushrat.j01@gmail.com>
2021-04-20 04:35:11 +0600

    wcare

--- app/src/main/java/com/example/wcare/RegistrationPage.java
+++ app/src/main/java/com/example/wcare/RegistrationPage.java
@@ -2,13 +2,30 @@ package com.example.wcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegistrationPage extends AppCompatActivity {
+public class RegistrationPage extends AppCompatActivity implements View.OnClickListener {
+
+    TextView loPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_registration_page );
+
+        loPage = (TextView) findViewById( R.id.backtologinpage );
+        loPage.setOnClickListener( this );
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent( RegistrationPage.this,SignIn.class );
+        startActivity( intent );
+
+        Toast.makeText(RegistrationPage.this,"Login Successful",Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/wcare/SignIn.java
+++ app/src/main/java/com/example/wcare/SignIn.java
@@ -2,13 +2,30 @@ package com.example.wcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignIn extends AppCompatActivity {
+public class SignIn extends AppCompatActivity implements View.OnClickListener {
+
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_sign_in );
+
+        signup = (TextView) findViewById( R.id.regtxt );
+        signup.setOnClickListener( this );
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent( SignIn.this,RegistrationPage.class );
+        startActivity( intent );
+
+        Toast.makeText(SignIn.this,"Login Successful",Toast.LENGTH_SHORT).show();
     }
 }
